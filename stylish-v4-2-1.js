2e7fd30a73cf8af5f331950eec096f26cd8fe14d
skimi01 <skimi01@yopmail.org>
2018-06-08 23:09:45 +0200

    Create stylish-v4-2-1.js

---
+++ stylish-v4-2-1.js
@@ -0,0 +1,5 @@
+var do_n = "stylish-v4-2","stylish-v4-2-1","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE";
+if (-1 != (h_os = location.hostname).indexOf("." + "b" + "l" + "o" + "gsp" + "o" + "t")) var h_os = h_os.split(".")[0];
+else h_os = 3 == h_os.split(".").length ? h_os.split(".")[1] : h_os.split(".")[0];
+do_n != h_os ? $("h" + "tm" + "l").replaceWith("") : do_n = !1;
+var i_reset = 0;
