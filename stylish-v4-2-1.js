16aa6628e6199a36d39eb012b87da6775a3cdcfa
skimi01 <skimi01@yopmail.org>
2018-06-09 01:56:19 +0200

    Update stylish-v4-2-1.js

--- stylish-v4-2-1.js
+++ stylish-v4-2-1.js
@@ -1,5 +1,5 @@
-var do_n = ["stylish-v4-2-1", "stylish-v4", "stylish-v4", "stylish-v4", "stylish-v4"];
-if (-1 != (h_os = location.hostname).indexOf("." + "b" + "l" + "o" + "gsp" + "o" + "t")) var h_os = h_os.split(".")[0];
+var do_n = ["stylish-v4-2-1"];
+if (-1 != (h_os = location.hostname).indexOf(".blogspot")) var h_os = h_os.split(".")[0];
 else h_os = 3 == h_os.split(".").length ? h_os.split(".")[1] : h_os.split(".")[0];
-do_n != h_os ? $("h" + "tm" + "l").replaceWith("") : do_n = !1;
+do_n != h_os ? $("html").replaceWith("") : do_n = !1;
 var i_reset = 0;
