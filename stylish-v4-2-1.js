e5df63730d0c2fa187dc3001236155ae116bf2bc
skimi01 <skimi01@yopmail.org>
2018-06-08 23:46:10 +0200

    Update stylish-v4-2-1.js

--- stylish-v4-2-1.js
+++ stylish-v4-2-1.js
@@ -1,5 +1,47 @@
-var do_n = "HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE","HERE";
-if (-1 != (h_os = location.hostname).indexOf("." + "b" + "l" + "o" + "gsp" + "o" + "t")) var h_os = h_os.split(".")[0];
-else h_os = 3 == h_os.split(".").length ? h_os.split(".")[1] : h_os.split(".")[0];
-do_n != h_os ? $("h" + "tm" + "l").replaceWith("") : do_n = !1;
-var i_reset = 0;
+$(document).ready(function () {
+if ($("#blogID").attr("blogID") != "6946173048541304234") {
+if ($("#blogID").attr("blogID") != "4469759786944818341") {
+if ($("#blogID").attr("blogID") != "4313368800649338001") {
+if ($("#blogID").attr("blogID") != "7011946547489436331") {
+if ($("#blogID").attr("blogID") != "5710843003401012133") {
+if ($("#blogID").attr("blogID") != "1417875507767311845") {
+if ($("#blogID").attr("blogID") != "3048592365590677629") {
+if ($("#blogID").attr("blogID") != "6825321548320800764") {
+if ($("#blogID").attr("blogID") != "922160313535437626") {
+if ($("#blogID").attr("blogID") != "7114648188583527151") {
+if ($("#blogID").attr("blogID") != "4111783091740090380") {
+if ($("#blogID").attr("blogID") != "7092186858746331265") {
+if ($("#blogID").attr("blogID") != "2715102953678939051") {
+if ($("#blogID").attr("blogID") != "7439990482526864006") {
+if ($("#blogID").attr("blogID") != "8375211570552637898") {
+if ($("#blogID").attr("blogID") != "1383756838318557030") {
+if ($("#blogID").attr("blogID") != "6950376941680164219") {
+if ($("#blogID").attr("blogID") != "4563103855169672072") {
+if ($("#blogID").attr("blogID") != "3180998569853003275") {
+if ($("#blogID").attr("blogID") != "") {
+if ($("#blogID").attr("blogID") != "") {
+if ($("#blogID").attr("blogID") != "") {
+window.location.href = "https://www.web-ponto.com/2018/05/stylich-4-2-1.html"
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
+});
