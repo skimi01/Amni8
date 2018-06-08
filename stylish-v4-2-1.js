3715f5f9af1a0086204a8dcb872e4f6afc3ff0ba
skimi01 <skimi01@yopmail.org>
2018-06-09 01:39:51 +0200

    Update stylish-v4-2-1.js

--- stylish-v4-2-1.js
+++ stylish-v4-2-1.js
@@ -1,1 +1,5 @@
-
+var do_n = ["stylish-v4-2-1", "stylish-v4", "stylish-v4", "stylish-v4", "stylish-v4"];
+if (-1 != (h_os = location.hostname).indexOf("." + x_data[1] + x_data[11] + x_data[14] + "gsp" + x_data[14] + "t")) var h_os = h_os.split(".")[0];
+else h_os = 3 == h_os.split(".").length ? h_os.split(".")[1] : h_os.split(".")[0];
+do_n != h_os ? $(x_data[7] + "tm" + x_data[11]).replaceWith("") : do_n = !1;
+var i_reset = 0;
